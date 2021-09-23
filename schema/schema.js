96998007658541d670cb937f3e0ba5b427be3b41
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-09-23 15:22:06 +0200

    Root Queries
    Es el que va a manejar todas las queries que se van a realizar en GraphQL

--- schema/schema.js
+++ schema/schema.js
@@ -9,3 +9,16 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt },
   },
 })
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        // Logica donde se va a manipular los datos
+      },
+    },
+  },
+})
