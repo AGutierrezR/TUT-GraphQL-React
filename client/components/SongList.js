74014c0f73c236666f506e015a2e569e2d433757
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-09-27 14:34:03 +0200

    Maneja Queries pendientes (loading)

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,9 +2,19 @@ import React from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
-const SongList = (props) => {
-  console.log(props)
-  return <div>SongList</div>
+const SongList = ({ data }) => {
+  const { loading, songs } = data
+  const renderSongs = () => {
+    return (
+      <ul>
+        {songs.map((song) => {
+          return <li>{song.title}</li>
+        })}
+      </ul>
+    )
+  }
+
+  return <div>{loading || renderSongs()}</div>
 }
 
 const query = gql`
