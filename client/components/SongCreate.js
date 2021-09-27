fa888566f888d13e1e6071f6a43eb74d2ab29e04
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-09-27 21:26:12 +0200

    Define Query Variables en React

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
+import { useMutation, gql } from '@apollo/client'
 
 const SongCreate = () => {
   const [title, setTitle] = useState('')
 
+  const [addSongMutation] = useMutation(mutation)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    addSongMutation({
+      variables: {
+        title,
+      },
+    })
+  }
+
   return (
     <div>
       <h3>Create a New Song</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <label htmlFor="">Song Title:</label>
         <input
           type="text"
@@ -17,5 +30,14 @@ const SongCreate = () => {
     </div>
   )
 }
+
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`
 
 export default SongCreate
