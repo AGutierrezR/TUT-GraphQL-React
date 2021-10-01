0360083962d7cedc561dd94b357122a32b6a3323
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-01 21:58:26 +0200

    UPDATE: Actualizar Babel, Webpack, css-loader y HTML WEBPACK PLUGIN

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: process.env.NODE_ENV,
   entry: './client/index.js',
   output: {
     path: '/',
@@ -10,19 +12,23 @@ module.exports = {
   module: {
     rules: [
       {
-        use: 'babel-loader',
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        use: ['style-loader', 'css-loader'],
-        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        test: /\.css$/i,
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: 'client/index.html',
+      inject: 'body',
     }),
   ],
 }
