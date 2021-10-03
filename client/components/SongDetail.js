ec749f089d895f00ac37188a2a672caa9bb81aee
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-03 19:49:10 +0200

    Integra Router con GraphQL

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,6 +1,12 @@
+import { useQuery } from '@apollo/client'
 import React from 'react'
+import { GET_SONG } from '../queries/apolloQueries'
+import { useParams } from 'react-router-dom'
 
 const SongDetail = () => {
+  const { id } = useParams()
+  const { data, loading, error } = useQuery(GET_SONG, { variables: { id } })
+
   return (
     <div>
       <h3>Song Detail</h3>
