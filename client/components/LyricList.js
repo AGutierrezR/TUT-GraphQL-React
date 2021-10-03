118f94cef69247c6874aa51dee70c1282cac98d0
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-04 01:19:49 +0200

    Usa optimistic response de Apollo

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -5,20 +5,27 @@ import { LIKE_LYRIC } from '../queries/apolloQueries'
 const LyricList = ({ lyrics }) => {
   const [likeLyricMutation] = useMutation(LIKE_LYRIC)
 
-  const onLike = (id) => {
+  const onLike = ({ id, likes }) => {
     likeLyricMutation({
       variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1,
+        },
+      },
     })
   }
 
   const renderLyrics = () => {
     return lyrics.map(({ id, content, likes }) => {
-      console.log(likes)
       return (
         <li key={id} className="collection-item">
           {content}
           <span className="vote-box">
-            <i className="material-icons" onClick={() => onLike(id)}>
+            <i className="material-icons" onClick={() => onLike({ id, likes })}>
               thumb_up
             </i>
             {likes}
