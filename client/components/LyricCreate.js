b70d606e9fee1c38a887b62d912a577777739aee
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-04 00:25:10 +0200

    Crea add lyric to song mutation

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ADD_LYRIC_TO_SONG } from '../queries/apolloQueries'
 
-const LyricCreate = () => {
+const LyricCreate = ({ songId }) => {
+  const [content, setContent] = useState('')
+  const [addLyricMutation] = useMutation(ADD_LYRIC_TO_SONG)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label>Add a Lyric</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
     </form>
   )
 }
