b5d4c9a26a78f7b92e8b4cbb9353625959d9d9f8
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-03 19:49:55 +0200

    Buscando datos

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -7,9 +7,14 @@ const SongDetail = () => {
   const { id } = useParams()
   const { data, loading, error } = useQuery(GET_SONG, { variables: { id } })
 
+  if (loading) return <p>Loading...</p>
+  if (!data?.song) return <p>Not found</p>
+
+  const { song } = data
+
   return (
     <div>
-      <h3>Song Detail</h3>
+      <h3>{song.title}</h3>
     </div>
   )
 }
