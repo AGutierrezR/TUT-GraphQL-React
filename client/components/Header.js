4bc6bb6ea2127d86500f5d5e9bac640ec580e227
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-07 01:13:50 +0200

    Authentication State

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,34 @@
-import { useQuery } from '@apollo/client'
 import React from 'react'
+import { useQuery } from '@apollo/client'
+
 import { FETCH_USER } from '../queries/CurrentUser'
 
 const Header = () => {
-  const { data, loading } = useQuery(FETCH_USER)
+  const { data, loading, error } = useQuery(FETCH_USER)
 
-  console.log({ data, loading })
+  const renderButtons = () => {
+    if (loading) {
+      return <div />
+    }
 
-  return <div>Header</div>
+    if (error) {
+      return <p>Error: {error.message}</p>
+    }
+
+    const { user } = data
+
+    if (user) {
+      return <div>Logout</div>
+    }
+
+    return <div>You&apos;re not signed in.</div>
+  }
+
+  return (
+    <nav>
+      <div className="nav-wrapper">{renderButtons()}</div>
+    </nav>
+  )
 }
 
 export default Header
