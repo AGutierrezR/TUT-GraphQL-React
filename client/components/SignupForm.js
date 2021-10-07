24b84c2d90803b07408ae7f6cf67f935baa0ad34
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-07 14:53:21 +0200

    Maneja de Errores en SignupForm

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { SIGNUP } from '../queries'
+import { FETCH_USER, SIGNUP } from '../queries'
 
 import AuthForm from './AuthForm'
 
 const SignupForm = () => {
   const [singupMutation] = useMutation(SIGNUP)
+  const [errors, setErrors] = useState([])
 
   const onSubmit = ({ email, password }) => {
     singupMutation({
       variables: { email, password },
+      refetchQueries: [{ query: FETCH_USER }],
+    }).catch((res) => {
+      const errors = res.graphQLErrors.map((error) => error.message)
+      setErrors([...errors])
     })
   }
 
   return (
     <div>
       <h3>Sign Up</h3>
-      <AuthForm onSubmit={onSubmit} />
+      <AuthForm onSubmit={onSubmit} errors={errors} />
     </div>
   )
 }
